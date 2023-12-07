10d32e552e05909862dd70bef7721927845231d9
radoncreep <radoncreep@gmail.com>
2023-12-07 18:22:39 +0100

    config change

--- apps/restaurant-service/drizzle.config.ts
+++ apps/restaurant-service/drizzle.config.ts
@@ -1,13 +1,21 @@
 import type { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config();
 
+console.log("name ",{
+    user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOSTNAME,
+        port: +process.env.POSTGRES_DB_PORT,
+        database: process.env.POSTGRES_DB_NAME
+})
 export default {
     schema: "./src/schema/*",
     driver: "pg",
     dbCredentials: {
-        user: "postgres",
-        host: "127.0.0.1",
-        port: 5432,
-        database: "restaurant"
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOSTNAME,
+        port: +process.env.POSTGRES_DB_PORT,
+        database: process.env.POSTGRES_DB_NAME
     },
     out: "./migrations"
 } satisfies Config;
